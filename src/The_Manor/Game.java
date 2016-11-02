dcb2261588310ee0fac4d4436b4a39fd4f754d2b
Aurelien <Aurelien@172.17.76.80>
2016-11-02 18:22:07 +0100

    Suppression des tests

--- src/The_Manor/Game.java
+++ src/The_Manor/Game.java
@@ -3,13 +3,12 @@ package The_Manor;
 public class Game {
 	
 	public Game(){
-		System.out.println("Vous avez cree une object de type Game");
+		System.out.println("Vous avez cree une objet de type Game");
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Game g = new Game();
-		System.out.println("Test 4");
 	}
 
 }
