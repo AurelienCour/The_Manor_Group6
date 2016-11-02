49a6eb2bfa9c3b9d8599e541b7741d8aa0346799
Aurelien <Aurelien@172.17.76.80>
2016-11-02 18:08:01 +0100

    Add the constructor for the class Game

--- src/The_Manor/Game.java
+++ src/The_Manor/Game.java
@@ -1,10 +1,14 @@
 package The_Manor;
 
 public class Game {
+	
+	public Game(){
+		System.out.println("Vous avez cree une object de type Game");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Game g = new Game();
 	}
 
 }
