111c7581c7efbe7e1292be223d20a1f588ae0f67
Astemir <tairax.turbo@hotmail.fr>
2016-11-10 09:42:09 +0100

    Test Astem

--- src/The_Manor/Game.java
+++ src/The_Manor/Game.java
@@ -3,7 +3,7 @@ package The_Manor;
 public class Game {
 	
 	public Game(){
-		System.out.println("Vous avez cree une objet de type Game");
+		System.out.println("Vous avez cree une objet de type Astem");
 	}
 
 	public static void main(String[] args) {
