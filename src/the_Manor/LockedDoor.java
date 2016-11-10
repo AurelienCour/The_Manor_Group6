bc0dc2443f3afcde7ca088535fd0eada2af15e94
Aurelien <Aurelien@172.17.68.122>
2016-11-10 18:40:54 +0100

    Change for door

--- src/the_Manor/LockedDoor.java
+++ src/the_Manor/LockedDoor.java
@@ -2,4 +2,8 @@ package the_Manor;
 
 public class LockedDoor extends Door{
 
+	public LockedDoor(Room nextRoom) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- src/the_Manor/Room.java
+++ src/the_Manor/Room.java
@@ -1,5 +1,69 @@
 package the_Manor;
 
-public class Room {
+import java.util.*;
 
+public class Room {
+	private HashMap<String,Door> exitPossible;
+	private String roomName;
+	private Character enemy;
+	private Character ally;
+	private Item objectInRoom;
+	
+	/**
+	 * The constructor of the class Room
+	 * @param roomName The name of the Room
+	 */
+	public Room (String roomName){
+		this.roomName = roomName;
+		this.exitPossible = new HashMap();
+	}
+	
+	/**
+	 * Allows to add an exit to the room
+	 * @param direction The direction for the door (East,West,South,North)
+	 * @param lock A boolean to know if the door is locked or not
+	 */
+	public void addExit(String direction,boolean lock, Room nextRoom){
+		if(!this.exitPossible.containsKey(direction)){
+			if(lock)
+				this.exitPossible.put(direction, new LockedDoor(nextRoom));
+			else
+				this.exitPossible.put(direction, new Door());
+		}
+		else
+			System.out.println("Erreur");
+	}
+	
+	/**
+	 * Allows to add an Enigmatic door to the room
+	 * @param direction
+	 */
+	public void addEnigmaticExit(String direction,String enigma,String response,Room nextRoom){
+		if(!this.exitPossible.containsKey(direction))
+			this.exitPossible.put(direction, new EnigmaticDoor(enigma,response,nextRoom));
+		else
+			System.out.println("Erreur");
+	}
+	
+	/**
+	 * @return The name of the room
+	 */
+	public String getName(){
+		return this.roomName;
+	}
+	
+	/**
+	 * @return If an enemy is in the room
+	 */
+	public Character getEnemy(){
+		return this.enemy;
+	}
+	
+	/**
+	 * @return If an ally is in the room
+	 */
+	public Character getAlly(){
+		return this.ally;
+	}
+	
 }
