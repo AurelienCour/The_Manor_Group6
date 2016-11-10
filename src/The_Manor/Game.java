ba9f0bce4b25585402a93d09ffff8f3183916131
Astemir <tairax.turbo@hotmail.fr>
2016-11-10 15:45:37 +0100

    Astem

--- src/The_Manor/Game.java
+++ src/The_Manor/Game.java
@@ -10,4 +10,22 @@ public class Game {
 		// TODO Auto-generated method stub
 		GameTest g = new GameTest();
 	}
+	
+	// metode qui marche
+	public static int calculer(int a, int b) {
+		    int res = a + b;
+		    
+		    if (a == 0){
+		      res = b * 2;
+		    } 
+		    
+		    if (b == 0) {
+		      res = a * a;     
+		    }
+		    return res; 
+		  }
+	
+	
+	
+	
 }
--- src/The_Manor/GameTest.java
+++ src/The_Manor/GameTest.java
@@ -8,27 +8,33 @@ public class GameTest extends TestCase
 	
 	@BeforeClass
     public static void setUpClass() throws Exception {
-        // Code exécuté avant l'exécution du premier test (et de la méthode @Before)        
+        // Code execution avant l'execution du premier test (et de la methode @Before)        
     }
 
     @AfterClass
     public static void tearDownClass() throws Exception {
-        // Code exécuté après l'exécution de tous les tests
+        // Codeexecution apres l'execution de tous les tests
     }
 
     @Before
     public void setUp() throws Exception {
-        // Code exécuté avant chaque test        
+        // Code execution avant chaque test        
     }
 
     @After
     public void tearDown() throws Exception {
-        // Code exécuté après chaque test
+        // Code execution apres chaque test
     }
     
     @Test
     public void testConstructor() {
        this.game1 = new Game();
     }
+    
+    
+    @Test
+    public void testCalculer() throws Exception {
 
+        assertEquals(2,Game.calculer(1,1));
+      }
 }
