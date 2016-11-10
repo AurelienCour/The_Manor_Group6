589dabc8b7e0e0f6b601c6a0aff5c3116a6889d2
Astemir <tairax.turbo@hotmail.fr>
2016-11-10 17:21:44 +0100

    Update

--- src/the_Manor/Ally.java
+++ src/the_Manor/Ally.java
@@ -2,4 +2,9 @@ package the_Manor;
 
 public class Ally extends Character{
 
+	public Ally(String newName, Room InitialRoom) {
+		super(newName, InitialRoom);
+		// TODO Auto-generated constructor stub
+	}
+
 }
