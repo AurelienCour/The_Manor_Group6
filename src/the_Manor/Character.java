feaf3323937d1af8467fda1664fd9e08f6a10108
Astemir <tairax.turbo@hotmail.fr>
2016-11-22 09:30:13 +0100

    AS

--- src/the_Manor/Character.java
+++ src/the_Manor/Character.java
@@ -4,6 +4,7 @@ package the_Manor;
  * <ul>A character has : </ul>
  * <li> a name : a string that will be defined
  * <li> a description : a small description of the character (string)
+ * <li> The position of the character : the CurrentRoom variable
  * @author astem
  * @version 22/11/2016
  *
