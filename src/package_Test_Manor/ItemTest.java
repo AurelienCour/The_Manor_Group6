ef8ff7e976a1d2b2da9c20fb977a47c67935b269
Aurelien <Aurelien@172.17.68.122>
2016-11-22 16:48:22 +0100

    Commentaire ItemTest

--- src/package_Test_Manor/ItemTest.java
+++ src/package_Test_Manor/ItemTest.java
@@ -10,6 +10,12 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
+/**
+ * The test class ItemTest
+ * Unit tests of the Item class
+ * @author Aurelien
+ *
+ */
 public class ItemTest {
 	
 	private Shield shield;
@@ -21,6 +27,10 @@ public class ItemTest {
 		
 	}
 	
+	/**
+	 * Sets up the test fixture
+	 * @throws Exception
+	 */
     @Before
     public void setUp() throws Exception {
         this.shield = new Shield("bouclier",15);
@@ -34,21 +44,33 @@ public class ItemTest {
         // Code execute apres chaque testf
     }
     
+    /**
+     * Test the getter for an object of type Shield
+     */
     @Test
     public void testGetNameShield(){
     	assertEquals("bouclier",this.shield.getName());
     }
     
+    /**
+     * Test the getter for an object of type Weapon
+     */
     @Test
     public void testGetNameWeapon(){
     	assertEquals("epee",this.weapon.getName());
     }
     
+    /**
+     * Test the getter for an object of type Potion
+     */
     @Test
     public void testGetNamePotion(){
     	assertEquals("potion",this.potion.getName());
     }
     
+    /**
+     * Test the getter for an object of type Key
+     */
     @Test
     public void testGetNameKey(){
     	assertEquals("clef",this.key.getName());
