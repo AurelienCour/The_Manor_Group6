5cd1b0fbc2ddf32d89ec798f0d5d359ea5a520d8
Aurelien <Aurelien@172.17.68.122>
2016-11-22 14:33:07 +0100

    DoorTest Aurelien

--- src/package_Test_Manor/DoorTest.java
+++ src/package_Test_Manor/DoorTest.java
@@ -5,6 +5,12 @@ import org.junit.*;
 import the_Manor.Door;
 import the_Manor.Room;
 
+/**
+ * The test class DoorTest
+ * Unit tests of the Door class
+ * @author Aurelien
+ *
+ */
 public class DoorTest {
 	private Room r;
 	private Door d;
