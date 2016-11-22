7d2ee8de07b387bea528100dee09a5f949b47031
Aurelien <Aurelien@172.17.68.122>
2016-11-22 14:39:37 +0100

    EnemyTest Aurelien

--- src/package_Test_Manor/EnemyTest.java
+++ src/package_Test_Manor/EnemyTest.java
@@ -4,30 +4,28 @@ import static org.junit.Assert.*;
 import the_Manor.Enemy;
 import org.junit.*;
 
+/**
+ * The test class EnemyTest
+ * Unit tests of the Enemy class
+ * @author Aurelien
+ *
+ */
 public class EnemyTest {
-	@BeforeClass
-    public static void setUpClass() throws Exception {
-        // Code execute avant l'exécution du premier test (et de la méthode @Before)        
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-        // Code execute après l'exécution de tous les tests
-    }
-
+	
+	private Enemy enemy;
+	
+	public EnemyTest(){
+		
+	}
+	
     @Before
     public void setUp() throws Exception {
-        // Code execute avant chaque test        
+    	this.enemy = new Enemy("Toto");
     }
 
     @After
     public void tearDown() throws Exception {
         // Code execute apres chaque test
     }
-    
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
 }
