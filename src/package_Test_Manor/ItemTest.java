da1ce37a7b6fa016eef57a3363bd978c57ccec5b
Aurelien <Aurelien@172.17.68.122>
2016-11-22 15:31:37 +0100

    ItemTest Aurelien

--- src/package_Test_Manor/ItemTest.java
+++ src/package_Test_Manor/ItemTest.java
@@ -10,39 +10,15 @@ public class ItemTest {
 	private String name;
 	
 	
-	@BeforeClass
-    public static void setUpClass() throws Exception {
-        // Code execute avant l'exécution du premier test (et de la méthode @Before)        
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-        // Code execute après l'exécution de tous les tests
-    }
 
     @Before
     public void setUp() throws Exception {
-        this.item1 = new Item(name);       
+        // 
     }
 
     @After
     public void tearDown() throws Exception {
         // Code execute apres chaque test
-    }
-    
-    @Test
-    public void testConstructor() {
-    	Item item2 = new Item(name);
-    }
-    
-    @Test
-    public void testSetName() {
-    	this.item1.setName();
-    }
-    
-    @Test
-    public void testGetName() {
-    	this.item1.getName();
     }
 
 }
