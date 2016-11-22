200e0cd979098930bec372e14b7ca487f3260864
Willy <dieua@172.17.65.174>
2016-11-22 09:19:12 +0100

    New specifications for Ally and Ennemy class

--- src/the_Manor/Enemy.java
+++ src/the_Manor/Enemy.java
@@ -1,5 +1,11 @@
 package the_Manor;
-
+/**
+ * <p>This class represents all the characters who are enemies of the main character.</p>
+ * <p>For the moment, this class does not contain methods, it is just representative</p>
+ * 
+ * @author Willy Dieuaide
+ * @version 2016-22-11
+ */
 public class Enemy extends Fighter{
 
 	/**
