899c03fe34a2dabfb5c6330036b29d758ec08ec3
Aurelien <Aurelien@172.17.68.122>
2016-11-24 09:31:22 +0100

    Change room. Aurelien

--- src/package_Test_Manor/RoomTest.java
+++ src/package_Test_Manor/RoomTest.java
@@ -175,32 +175,4 @@ public class RoomTest
 		myRoom.addExit(direction, false, myRoom);				
 		assertEquals(myDoor, myRoom.getDoor("direction1"));
 	}
-
-
-	/**
-	@BeforeClass
-    public static void setUpClass() throws Exception {
-        // Code execute avant l'exécution du premier test (et de la méthode @Before)        
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-        // Code execute après l'exécution de tous les tests
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        // Code execute avant chaque test        
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        // Code execute apres chaque test
-    }
-    
-    @Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-	*/
 }
