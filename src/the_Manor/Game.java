b08a61c9b6e35e0fa050b730a5c5ec1257080022
sihambennis <sihambennis@pc55.home>
2016-12-03 11:48:09 +0100

    test commit

--- src/the_Manor/Game.java
+++ src/the_Manor/Game.java
@@ -143,7 +143,7 @@ public class Game {
 		//floor 2
 		corridor2_1.addEnigmaticExit("SUD","I have something in my pocket, but my pocket is empty. What is it?","hole",bathroom2);
 		corridor2_2.addEnigmaticExit("NORD","Who is the supreme commander of The Manor?","Aurelien",gameRoom2);
-        corridor2_3.addEnigmaticExit("NORD","I have a father but I am not his son and I have a mother but I am not her son. Who am I?","daughter",sisterRoom2);
+        corridor2_3.addEnigmaticExit("NORD","I have a father but I am not his son. I have a mother but I am not her son. Who am I?","daughter",sisterRoom2);
 		
 
         // ITEM AND CHARACTER
