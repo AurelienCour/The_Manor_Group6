7916dba0bf76b7afb659e82f3c03576a8a630eb5
Simon Hay <simon.hay@hotmail.com>
2016-12-03 12:02:59 +0100

    Addition of comments

--- src/the_Manor/Fight.java
+++ src/the_Manor/Fight.java
@@ -2,24 +2,50 @@ package the_Manor;
 
 import package_Display.WindowDisplayMessage;
 
+/**
+ * <p>This class allow to make a fight happens between the player and an enemy</p>
+ * <p>As the player enters in a room, if there is a enemy in this room, the figth is lauched</p>
+ * <p></p>
+ * @author Group 6
+ *
+ */
 public class Fight {
 	
 	private Enemy enemy;
 	private Player player;
 	
+	/**
+	 * The constructor of the class Fight
+	 * @param player The player of the game
+	 * @param enemy The enemy encountered by the player
+	 */
 	public Fight(Player player,Enemy enemy){
 		this.enemy = enemy;
 		this.player = player;
 	}
 	
+	/**
+	 * To know who is the player in the room
+	 * @return The player in the room
+	 */
 	public Player getPlayer(){
 		return this.player;
 	}
 	
+	/**
+	 * To know who is the enemy in the room
+	 * @return The enemy in the room
+	 */
 	public Enemy getEnemy(){
 		return this.enemy;
 	}
 	
+	/**
+	 * <p>Allow a fighter to launch an attack</p>
+	 * <p>If the fighter is a player and he has enough stamina, he will remove some health of the enemy but his stamina will goes down according to the power of his attack</p>
+	 * <p>If the fighter is an enemy and he has enough stamina, he will remove some health of the player but his stamina will goes down according to the power of his attack</p>	
+	 * @param fight The fighter that launches the attack
+	 */
 	public void attack(Fighter fight){
 		if(fight instanceof Player){
 			if(this.player.getStamina()>0){
@@ -38,6 +64,10 @@ public class Fight {
 		}
 	}
 	
+	/**
+	 * Allow for a fighter to get some stamina back
+	 * @param fighter The fighter that wants to get some stamina back
+	 */
 	public void recup(Fighter fighter){
 		fighter.addStamina(fighter.getAttack());
 	}
